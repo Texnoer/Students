9d4feebe526c339175698a768390f2b44805f4de
texnoer <texnoer@ro.ru>
2020-12-21 19:38:33 +0300

    student_new1

--- Student.py
+++ Student.py
@@ -17,6 +17,14 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    pass
+# Получать оценки за лекции от студентов :)
+# Реализуйте метод выставления оценок лекторам у класса Student (оценки по 10-балльной шкале, хранятся в атрибуте-списке).
+# Лектор при этом должен быть закреплен за тем курсом, на который записан студент.
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
